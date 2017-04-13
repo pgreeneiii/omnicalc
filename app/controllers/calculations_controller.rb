26e01756e61feedc822cf0497ccf413a6867172a
Phil Greene <pgreeneiii@users.noreply.github.com>
2017-04-13 16:17:38 -0500

    Final Version
    Completed all exercises for omnicalc. Passes rails grade test 100%.

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,120 +1,188 @@
 class CalculationsController < ApplicationController
 
-  def word_count
-    @text = params[:user_text]
-    @special_word = params[:user_word]
+   def word_count
+      @text = params[:user_text]
+      @special_word = params[:user_word]
 
-   sentence = @text.downcase.split
-   search_key = @special_word.downcase.split
+      sentence = @text.downcase.split
+      search_key = @special_word.downcase.split
 
-   sentence_length = 0
-   search_count = 0
+      sentence_length = 0
+      search_count = 0
 
-   sentence.each do |word|
-      sentence_length += word.length
+      sentence.each do |word|
+         sentence_length += word.length
 
-      search_key.each do |key|
-         if key == word.gsub(/[^a-z0-9\s]/i, "")
-            search_count += 1
+         search_key.each do |key|
+            #remove special characters and count search words
+            if key == word.gsub(/[^a-z0-9\s]/i, "")
+               search_count += 1
+            end
          end
       end
+
+      @word_count = @text.split.count
+
+      @character_count_with_spaces = @text.length
+
+      @character_count_without_spaces = sentence_length
+
+      @occurrences = search_count
+
+      render("word_count.html.erb")
    end
 
-    @word_count = @text.split.count
+   def loan_payment
+      @apr = params[:annual_percentage_rate].to_f
+      @years = params[:number_of_years].to_i
+      @principal = params[:principal_value].to_f
 
-    @character_count_with_spaces = @text.length
+      monthly_interest = (@apr/12.0/100) # convert to monthly percentage rate
+      months = (@years*12) # convert to months
 
-    @character_count_without_spaces = sentence_length
+      payment = ((@principal*monthly_interest)/(1-(1+monthly_interest)**(-months))) #perform payment calculation
 
-    @occurrences = search_count
+      @monthly_payment = payment
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+      render("loan_payment.html.erb")
+   end
 
-    render("word_count.html.erb")
-  end
+   def time_between
+      @starting = Chronic.parse(params[:starting_time])
+      @ending = Chronic.parse(params[:ending_time])
 
-  def loan_payment
-    @apr = params[:annual_percentage_rate].to_f
-    @years = params[:number_of_years].to_i
-    @principal = params[:principal_value].to_f
+      seconds = @ending-@starting
+      minutes = (seconds/60.0)
+      hours = (minutes/60.0)
+      days = (hours/24.0)
+      weeks = (days/7.0)
+      years = (weeks/52.0)
 
-   monthly_interest = (@apr/12.0/100)
-   months = (@years*12)
+      @seconds = seconds
+      @minutes = minutes
+      @hours = hours
+      @days = days
+      @weeks = weeks
+      @years = years
 
-   payment = ((@principal*monthly_interest)/(1-(1+monthly_interest)**(-months)))
+      render("time_between.html.erb")
+   end
 
-    @monthly_payment = payment
+   def descriptive_statistics
+      @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
 
-    render("loan_payment.html.erb")
-  end
+      # Code for Sorting
+      sorted = @numbers.sort
 
-  def time_between
-    @starting = Chronic.parse(params[:starting_time])
-    @ending = Chronic.parse(params[:ending_time])
+      # Calculate Range
+      range = sorted.last-sorted.first
 
-    # ================================================================================
-    # Your code goes below.
-    # The start time is in the Time @starting.
-    # The end time is in the Time @ending.
-    # Note: Ruby stores Times in terms of seconds since Jan 1, 1970.
-    #   So if you subtract one time from another, you will get an integer
-    #   number of seconds as a result.
-    # ================================================================================
+      # Calculate Median
+      median_num = ((sorted.count-1)/2)
 
-    seconds = @ending-@starting
-    minutes = (seconds/60.0)
-    hours = (minutes/60.0)
-    days = (hours/24.0)
-    weeks = (days/7.0)
-    years = (weeks/52.0)
+      if median_num == 0
+         median = sorted[0]
+      #median calc is different when number of items is even
+      elsif median_num > 0 && (sorted.count%2) == 0
+         median = (sorted[median_num]+sorted[median_num+1])/2
+      #median calc for odd number of items
+      else
+         median = sorted[median_num]
+      end
 
-    @seconds = seconds
-    @minutes = minutes
-    @hours = hours
-    @days = days
-    @weeks = weeks
-    @years = years
+      # Calculate Sum
+      sum = 0
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+      sorted.each do |num|
+         sum += num
+      end
 
-    render("time_between.html.erb")
-  end
+      # Calculate Mean
+      n = @numbers.count*1.0 #make n floating number
+      mean = (sum/n)
 
-  def descriptive_statistics
-    @numbers = params[:list_of_numbers].gsub(',', '').split.map(&:to_f)
+      # Calculate Variance
+      variance_numerator = 0
+      sorted.each do |num|
+         variance_numerator += ((num-mean)**2)
+      end
 
-   
+      variance = (variance_numerator/n)
 
-    @sorted_numbers = "Replace this string with your answer."
+      # Calculate Standard Deviation
+      sd = (variance)**(1.0/2.0)
 
-    @count = "Replace this string with your answer."
+      # Calculate Mode
+      unique_numbers = []
+      sorted.each do |num| #build array of unique numbers
+         if unique_numbers == []
+            unique_numbers.push(num)
+            next
 
-    @minimum = "Replace this string with your answer."
+         else
+            check = 0
+            unique_numbers.each do |test|
 
-    @maximum = "Replace this string with your answer."
+               if test == num
+                  check = 1
+               else
+                  next
+               end
+            end
 
-    @range = "Replace this string with your answer."
+            if check == 0
+               unique_numbers.push(num)
+            end
+         end
+      end
 
-    @median = "Replace this string with your answer."
+      high_count = 0
+      mode = 0
+      unique_numbers.each do |num|
+         count = 0
 
-    @sum = "Replace this string with your answer."
+         sorted.each do |compare| #count number of occurences
+            if num == compare
+               count += 1
+            end
+         end
 
-    @mean = "Replace this string with your answer."
+         if count > high_count #store as mode if current highest count
+            high_count = count
+            mode = num
+         end
+      end
 
-    @variance = "Replace this string with your answer."
+      if unique_numbers.count == sorted.count
+         mode = 0
+      end
 
-    @standard_deviation = "Replace this string with your answer."
+      @sorted_numbers = sorted
 
-    @mode = "Replace this string with your answer."
+      @count = @numbers.count
 
-    # ================================================================================
-    # Your code goes above.
-    # ================================================================================
+      @minimum = sorted.first
 
-    render("descriptive_statistics.html.erb")
-  end
+      @maximum = sorted.last
+
+      @range = range
+
+      @median = median
+
+      @sum = sum
+
+      @mean = mean
+
+      @variance = variance
+
+      @standard_deviation = sd
+
+      @mode = mode
+
+      # ================================================================================
+      # Your code goes above.
+      # ================================================================================
+
+      render("descriptive_statistics.html.erb")
+   end
 end
